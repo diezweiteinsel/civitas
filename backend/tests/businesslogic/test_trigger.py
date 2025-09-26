4285601ef18df62d5c27fabd0bd1dcb16c9e4ae2
Jonathan Reuter <jonathan.s.reuter@web.de>
2025-09-26 15:04:54 +0200

    feat[tests]: added tests for all triggers. still needs content, but am too tired...

---
+++ backend/tests/businesslogic/test_trigger.py
@@ -0,0 +1,23 @@
+from backend.models.domain.trigger import TriggerType, TriggerResultType, LogicTriggerType, StringTriggerType, IntTriggerType, DateTriggerType, FloatTriggerType
+from backend.businesslogic.trigger import evaluateTrigger, evaluateTriggerLogicExpression, evaluateTriggerLogic, evaluateTriggerLogicDate, evaluateTriggerLogicFloat, evaluateTriggerLogicInt, evaluateTriggerLogicString, evaluateTriggerResult
+
+def test_evaluateTrigger():
+	pass
+
+def test_evaluateTriggerLogicExpression():
+	pass
+
+def test_evaluateTriggerLogic():
+	pass
+
+def test_evaluateTriggerLogicDate():
+	pass
+
+def test_evaluateTriggerLogicInt():
+	pass
+
+def test_evaluateTriggerLogicFloat():
+	pass
+
+def test_evaluateTriggerLogicString():
+	pass

8f2f8e601943e25e9f9d0c2ab8b588f902ac7095
Jaysun <jaysunwestenberg@yahoo.de>
2025-09-26 20:38:30 +0200

    feat: removed mocking and local database

--- frontend/src/pages/admin/Dashboard.jsx
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -4,31 +4,19 @@ import Navbar from "./../../components/Navbar";
 import { FaDog, FaFire, FaInfoCircle } from "react-icons/fa";
 import { Role } from "../../utils/const";
 import ApplicationContainer from "./../../components/ApplicationContainer";
-import { getAllApplications, getApplicationsByStatus } from "../../utils/data";
 
 export default function AdminPage() {
   const [pendingApplications, setPendingApplications] = useState([]);
   const [approvedApplications, setApprovedApplications] = useState([]);
 
-  useEffect(() => {
-    const pending = getApplicationsByStatus("pending");
-    const approved = getApplicationsByStatus("approved");
-
-    setPendingApplications(pending);
-    setApprovedApplications(approved);
-  }, []);
-
   return (
     <>
       <Navbar role={Role.ADMIN} />
       <div className="page-container">
-        <ApplicationContainer
-          applications={pendingApplications}
-          title="Pending Applications:"
-        />
+        <ApplicationContainer applications={[]} title="Pending Applications:" />
         <div className="page-space"> </div>
         <ApplicationContainer
-          applications={approvedApplications}
+          applications={[]}
           title="Approved Applications:"
         />
       </div>

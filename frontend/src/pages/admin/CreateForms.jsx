fd30b9c90459556e5e79eb8e4933558d67041e4a
isha-sudhakar <isha.sudhakar.rodriguez@gmail.com>
2025-09-27 11:16:02 +0200

    chore: minor fixes to the functions

--- frontend/src/pages/admin/CreateForms.jsx
+++ frontend/src/pages/admin/CreateForms.jsx
@@ -1,17 +1,17 @@
 // pflichtenheft-user-interface mockup under "Editing a from/Adding trigger-conditionals"
 // Diese Seite soll "ViewForms.jsx" aufgerufen werden und soll die Möglichkeit bieten, ein neues Formular zu erstellen.
 
-import React from "react";
+import React, { useState } from "react";
 import "./../../style/AdminApplicantReporterPage.css";
 
 export default function Forms() {
-    const [formFields, setFormFields] = usestate([
-        {fieldOne: "", fieldTwo: ""},
+    const [formFields, setFormFields] = useState([
+        {fieldOne: "", fieldTwo: ""}, // needs to be implemnted as a dict instead
     ])
 
     const handleFormChange = (event, index) => {
         let data = [...formFields]
-        data[index][event.target.fieldOne] = event.target.value
+        data[index][event.target.name] = event.target.value
         setFormFields(data)
     }
 
@@ -20,13 +20,17 @@ export default function Forms() {
             fieldOne: "",
             fieldTwo: ""
         }
-
-        setFormFields(...formFields, [object]) //ensures that previous object isn't overwritten 
+        setFormFields([...formFields, object]) //ensures that previous object doesn't get overwritten
     }
 
     const removeFields = (index) => {
         let data = [...formFields]
-        data.splice[index,1]
+        data.splice(index,1)
         setFormFields(data)
     }
+
+    return (
+        <>
+        </>
+    )
 }

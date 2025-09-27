d7fd8236b460fa4f866dad9879731e9a4bbc8491
isha-sudhakar <isha.sudhakar.rodriguez@gmail.com>
2025-09-27 13:50:12 +0200

    fix: revamp logic and fix add function

--- frontend/src/pages/admin/CreateForms.jsx
+++ frontend/src/pages/admin/CreateForms.jsx
@@ -5,50 +5,91 @@ import React, { useState } from "react";
 import "./../../style/CreateEditForms.css";
 import "./../../style/AdminApplicantReporterPage.css"
 
+const dataTypes = ["String", "Date", "Float", "Boolean", "Number"];
+
 export default function Forms() {
     const [formFields, setFormFields] = useState([
         { id: '1', name: "Feld eins", type: "Text", value: "" }, // needs to be implemnted as a dict instead? i dont know 
     ])
 
-    const handleFormChange = (event, index) => {
-        let data = [...formFields]
-        data[index][event.target.name] = event.target.value
-        setFormFields(data)
-    }
+    const getNextId = () => (Date.now() + Math.random()).toString(); 
+
+    const handleForm = (event, index) => {
+        const { name, value } = event.target;
+        let data = [...formFields];
+        data[index][name] = value;
+        setFormFields(data);
+    };
 
     const addFields = () => {
-        let object = {
-            fieldOne: "",
-            //fieldTwo: ""
-        }
-        setFormFields([...formFields, object]) //ensures that previous object doesn't get overwritten
-    }
+        let newField = {
+            id: getNextId(),
+            name: `Neues Feld ${formFields.length + 1}`, 
+            type: "String", 
+            value: ""
+        };
+        setFormFields([...formFields, newField]);
+    };
 
-    const removeFields = (index) => {
-        let data = [...formFields]
-        data.splice(index,1)
-        setFormFields(data)
-    }
+    const removeFields = (id) => {
+        const data = formFields.filter(field => field.id !== id);
+        setFormFields(data);
+    };
 
     const handleImport = () => {
-
+        alert("Meldeform wurde erflogreich importiert") //TODO:
     }
 
     return (
-        <>
-        <Navbar />
         <div>
-            <button 
-            className="create-button"
-            onClick={() => addFields()}
-            >Neue Meldeform Erstellen
-            </button>
-            <button 
-            className="import-button"
-            onClick={() => handleImport()}
-            >Meldeform Importieren
-            </button>
+            
+            <div>
+                <button 
+                    className="create-button"
+                    onClick={addFields}
+                >
+                    Neues Feld Hinzufügen 
+                </button>
+                <button 
+                    className="import-button"
+                    onClick={handleImport}
+                >
+                    Meldeform Importieren 
+                </button>
+            </div>
+            
+            <div>
+                
+                {formFields.map((field, index) => (
+                    <div key={field.id} className="field-row">
+
+                        <input 
+                            name="name"
+                            placeholder="Feldname"
+                            value={field.name}
+                            onChange={(e) => handleForm(e, index)}
+                        />
+
+                        <select
+                            name="type"
+                            value={field.type}
+                            onChange={(e) => handleForm(e, index)}
+                        >
+                            {dataTypes.map((type) => (
+                                <option key={type} value={type}>{type}</option>
+                            ))}
+                        </select>
+                        
+                        <button 
+                            className="remove-button"
+                            onClick={() => removeFields(field.id)} 
+                        >
+                            Entfernen 
+                        </button>
+                    </div>
+                ))}
+                
+            </div>
         </div>
-        </>
-    )
+    );
 }

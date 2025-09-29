a1dcff015c29b4ff8fb702b7efc128e19a635ccf
isha-sudhakar <isha.sudhakar.rodriguez@gmail.com>
2025-09-29 20:45:30 +0200

    chore: minor fixes

--- frontend/tests/ApplicationContainer.test.js
+++ frontend/tests/ApplicationContainer.test.js
@@ -2,6 +2,21 @@ import { render } from "@testing-library/react"
 import ApplicationContainer from "../src/components/ApplicationContainer";
 
 
+const mockApplication = [
+    {
+        id: 111,
+        formId: 1,
+        status: "pending"
+
+    },
+
+    {
+        id: 112,
+        forID: 2,
+        status: "rejected"
+    }
+]
+
 describe(ApplicationContainer, () => {
     
     it("correct icon is displayed based on form type", () => { //getIconByFormId
@@ -9,15 +24,3 @@ describe(ApplicationContainer, () => {
     })
 })
 
-const mockApplication = [
-    {
-        id: 111,
-        formId: 1
-
-    },
-
-    {
-        id: 112,
-        forID: 2
-    }
-]
